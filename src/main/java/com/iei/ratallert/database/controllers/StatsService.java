4ee10fc985ac062a6bb574acc1b7143d6d89d86a
Ihor Horchynskyi <i.horchynskyi@gmail.com>
2021-01-02 00:31:58 +0200

    adding some logs

--- src/main/java/com/iei/ratallert/database/controllers/StatsService.java
+++ src/main/java/com/iei/ratallert/database/controllers/StatsService.java
@@ -2,9 +2,11 @@ package com.iei.ratallert.database.controllers;
 
 import com.iei.ratallert.database.entities.Stat;
 import com.iei.ratallert.database.repository.StatsRepository;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@Log4j2
 @Service
 public class StatsService {
 
@@ -12,6 +14,7 @@ public class StatsService {
     StatsRepository<Stat> statsRepository;
 
     public void save(Stat stat) {
+        log.debug("Saving stat: " + stat);
         statsRepository.saveAndFlush(stat);
     }
 }
--- src/main/java/com/iei/ratallert/rest/Arduino.java
+++ src/main/java/com/iei/ratallert/rest/Arduino.java
@@ -2,10 +2,12 @@ package com.iei.ratallert.rest;
 
 import com.iei.ratallert.database.controllers.StatsService;
 import com.iei.ratallert.database.entities.Stat;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Log4j2
 @RestController
 public class Arduino {
     @Autowired
@@ -13,6 +15,7 @@ public class Arduino {
 
     @PostMapping(path = "/v1/api/esp")
     public void getData(Stat stat){
+        log.debug("Getting data from endpoint: " + stat);
         statsService.save(stat);
     }
 }
