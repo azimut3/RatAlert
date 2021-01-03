e9b35785abfecdadc484e45aa2969896aa43de90
Ihor Horchynskyi <i.horchynskyi@gmail.com>
2021-01-03 14:40:46 +0200

    working prototype

--- src/main/java/com/iei/ratallert/rest/Arduino.java
+++ src/main/java/com/iei/ratallert/rest/Arduino.java
@@ -1,6 +1,7 @@
 package com.iei.ratallert.rest;
 
 import com.iei.ratallert.database.controllers.StatsService;
+import com.iei.ratallert.database.entities.HourlyStat;
 import com.iei.ratallert.database.entities.Stat;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,7 +22,7 @@ public class Arduino {
     }
 
     @PostMapping(path = "/v1/api/esp")
-    public String getData(@RequestBody Stat stat){
+    public String getData(@RequestBody HourlyStat stat){
         log.info("Getting data from endpoint: " + stat);
         statsService.save(stat);
 
